35963a0ac66d90edeb9ddae2d550c9216ffc7272
warakorn <boydoubleb@gmail.com>
2021-11-24 20:56:47 +0700

    [add]: about us screen

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,47 +1,100 @@
-import React, { useEffect, useState } from "react";
+import {
+  HeadingText2,
+  HeadingText3,
+  HeadingText4,
+} from "@components/typography/Typography";
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import {
+  LinkedinOutlined,
+  FacebookOutlined,
+  GithubOutlined,
+} from "@ant-design/icons";
+
+import Boy from "../assets/images/boy.jpeg";
+import Ohn from "../assets/images/ohn.jpeg";
+import Paint from "../assets/images/paint.jpeg";
+
+const DEVELOPERS_TEAMS = [
+  {
+    img: Boy,
+    name: "Warakorn Chantranupong",
+    pos: "Fullstack Developer",
+    fUrl: "https://www.facebook.com/boykrubb/",
+    lUrl: "https://www.linkedin.com/in/warakorn-chantranupong-3305911b7/",
+    gUrl: "https://github.com/boy-warakorn",
+  },
+  {
+    img: Paint,
+    name: "Anawat Paothong",
+    pos: "Mobile Developer",
+    fUrl: "https://www.facebook.com/anawat.master",
+    lUrl: "https://www.linkedin.com/in/anawat-paothong-a758b41b1/",
+    gUrl: "https://www.facebook.com/onnop.ohnnop",
+  },
+  {
+    img: Ohn,
+    name: "Noppanut Boonrueng",
+    pos: "Mobile Developer",
+    fUrl: "https://www.facebook.com/onnop.ohnnop",
+    lUrl: "https://www.facebook.com/onnop.ohnnop",
+    gUrl: "https://www.facebook.com/onnop.ohnnop",
+  },
+];
+
+const ICON_STYLE = { fontSize: "24px" };
 
 const AboutPage = () => {
-  const [start, setStart] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setStart(true);
-    }, 300);
-  }, []);
-
   return (
     <div
-      className="grid grid-cols-4 overflow-hidden px-6 lg:px-24"
+      className="lg:overflow-hidden xl:px-24 xl:py-6"
       style={{ height: "90vh" }}
     >
-      <div
-        className={`border-2 border-black hidden transform place-self-center self-end bg-primary lg:grid grid-cols-2 gap-6 p-6 ${
-          start ? "translate-y-0" : "translate-y-full"
-        }`}
-        style={{
-          height: "80vh",
-          width: "80%",
-          transition: "all 3s ",
-        }}
+      <ScrollAnimation
+        className="h-full w-full shadow-2xl bg-gray-100 px-16 py-12 rounded-lg flex flex-col items-center overflow-y-scroll"
+        animateIn="fadeInUp"
       >
-        {Array.from({ length: 14 }, (_, i) => i + 1).map(() => (
-          <div className="bg-white col-span-1 h-full border-2 border-black"></div>
-        ))}
-      </div>
-      <div
-        className={`border-2 border-black hidden transform place-self-center self-end bg-primary lg:grid grid-cols-2 gap-6 p-6 ${
-          start ? "translate-y-0" : "translate-y-full"
-        }`}
-        style={{
-          height: "60vh",
-          width: "70%",
-          transition: "all 3s ",
-        }}
-      >
-        {Array.from({ length: 12 }, (_, i) => i + 1).map(() => (
-          <div className="bg-white col-span-1 h-full border-2 border-black"></div>
-        ))}
-      </div>
-      <div className="" style={{ height: "100vh" }}></div>
+        <HeadingText2 className="text-center border-b-2 border-black w-max">
+          Our Team
+        </HeadingText2>
+        <div className=" w-full h-full mt-4 flex justify-between flex-col lg:flex-row">
+          {DEVELOPERS_TEAMS.map((dev) => (
+            <div className="flex flex-col items-center">
+              <div
+                className="rounded-full mt-12 shadow-2xl outline-img"
+                style={{
+                  height: "275px",
+                  width: "275px",
+                  background: `url('${dev.img}')`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+              <HeadingText3 className="mt-8">{dev.name}</HeadingText3>
+              <HeadingText4>- {dev.pos} -</HeadingText4>
+              <HeadingText4 className="mt-2 flex cursor-pointer">
+                <a href={dev.lUrl} target="_blank" rel="noreferrer">
+                  <LinkedinOutlined
+                    className="mr-2"
+                    style={{ ...ICON_STYLE, color: "#3B5998" }}
+                  ></LinkedinOutlined>
+                </a>
+                <a href={dev.fUrl} target="_blank" rel="noreferrer">
+                  <FacebookOutlined
+                    style={{ ...ICON_STYLE, color: "#0E76A8" }}
+                  />
+                </a>
+                <a href={dev.gUrl} target="_blank" rel="noreferrer">
+                  <GithubOutlined
+                    className="ml-2"
+                    style={{ ...ICON_STYLE, color: "#171515" }}
+                  />
+                </a>
+              </HeadingText4>
+            </div>
+          ))}
+        </div>
+      </ScrollAnimation>
     </div>
   );
 };
